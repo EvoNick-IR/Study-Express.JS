bb4322534891c9a484296c25cee61f0f4eda1dd5
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-10-24 18:14:45 +0330

    some change and make some api for post user , get users and get only one user

--- src/index.js
+++ src/index.js
@@ -1,10 +1,55 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = 4000;
 
 app.listen(PORT, () => {
   console.log(`app is starts on port: ${PORT}`);
 });
 
-app.use(express.json);
-app.use(express.urlencoded);
+app.use(express.json());
+app.use(express.urlencoded());
+
+let users = [
+  {
+    name: "hoda",
+    lastname: "ahmai",
+    age: 36,
+    sex: "female",
+    admin: "true",
+  },
+  {
+    name: "mostafa",
+    lastname: "haghani",
+    age: 33,
+    sex: "male",
+    admin: "true",
+  },
+  {
+    name: "banoo",
+    lastname: "hajipoor",
+    age: 60,
+    sex: "female",
+    admin: "true",
+  },
+];
+
+app.get("/", (req, res) => {
+  // console.log(req);
+  res.status(200).send(users);
+});
+
+app.get("/:user", (req, res) => {
+  const user = req.params.user;
+  const foundUser = users.find((username) => username.name === user);
+  console.log(user);
+  if (foundUser) return res.status(200).send(foundUser);
+  else res.status(404).send("user not found");
+});
+
+app.post("/user", (req, res) => {
+  const User = req.body;
+
+  users.push(User);
+
+  res.status(201).send(users);
+});
